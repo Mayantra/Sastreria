0208734e69ba53c2f8feb64975ba559c01efde10
Mayantra <isaac.vassquez@gmail.com>
2024-02-14 16:33:49 -0600

    Inicio de las ventanas principales

--- LoginSasteria/obj/Debug/LoginSasteria_Content.g.i.cs
+++ LoginSasteria/obj/Debug/LoginSasteria_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("grupo%2013.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("grupo%2015.png")]
 
 
