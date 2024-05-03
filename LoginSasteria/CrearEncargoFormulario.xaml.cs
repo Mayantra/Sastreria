c483e1fad24be27968d94e003024aa9849be6489
Mayantra <isaac.vassquez@gmail.com>
2024-05-03 16:46:13 -0600

    Elementos nuevos

--- LoginSasteria/CrearEncargoFormulario.xaml.cs
+++ LoginSasteria/CrearEncargoFormulario.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using iText.IO.Util;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +16,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-
 namespace LoginSasteria
 {
     /// <summary>
@@ -20,9 +23,11 @@ namespace LoginSasteria
     /// </summary>
     public partial class CrearEncargoFormulario : Window
     {
+        ConexionDB objConection = new ConexionDB();
         public CrearEncargoFormulario()
         {
             InitializeComponent();
+            tipoCon();
         }
         private void btnSalir(object sender, RoutedEventArgs e)
         {
@@ -32,6 +37,67 @@ namespace LoginSasteria
         private void Minimizar(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+        void tipoCon()
+        {
+            string query = "SELECT * FROM dbleonv2.tipoproducto;";
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(query, objConection.establecerCN());
+                MySqlDataReader myread;
+
+                myread = comando.ExecuteReader();
+
+                while (myread.Read())
+                {
+
+                    string nombres = myread.GetString("nombreTipo");
+                    CbTipo.Items.Add(nombres);
+
+                }
+                objConection.cerrarCN();
+                
+            }
+            catch (MySqlException x)
+            {
+                MessageBox.Show("Error: " + x);
+            }
+        }
+        private int ObtenerCantidad()
+        {
+            // Accede a la propiedad Value del control IntegerUpDown
+            int cantidad = NumberData.Value ?? 0; // Usa 0 como valor predeterminado si el valor es nulo
+            return cantidad;
+        }
+        private string ObtenerValorComboBox()
+        {
+            // Accede al valor seleccionado del ComboBox
+            string valorSeleccionadoCompleto = CbTipo.SelectedItem?.ToString(); // Obtén la cadena completa seleccionada
+            if (valorSeleccionadoCompleto != null)
+            {
+                // Divide la cadena en palabras
+                string[] palabras = Regex.Split(valorSeleccionadoCompleto, @"\W+");
+
+                // Toma las primeras dos palabras
+                string primerasDosPalabras = "";
+               
+                primerasDosPalabras = palabras[0] + palabras[1];
+                
+                return primerasDosPalabras;
+            }
+            else
+            {
+                MessageBox.Show("No se ha seleccionado ningún elemento en el ComboBox.");
+                return "";
+            }
+        }
+
+        private void AgregarProducto(object sender, RoutedEventArgs e)
+        {
+            int  cantidad = ObtenerCantidad();
+            string tipo = ObtenerValorComboBox();
+            MessageBox.Show("Elegiste "+tipo+" "+cantidad.ToString());
+
         }
     }
 }
